fd8c834888140428f865f964aa7d85c785a74f61
unknown <SChmutov@SCHMUTOV.dbdc.luxoft.com>
2016-05-21 15:58:55 +0300

    next iteration

--- JVA008/src/my/study/MyNetEcho2.java
+++ JVA008/src/my/study/MyNetEcho2.java
@@ -3,29 +3,88 @@ package my.study;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * Created by red2 on 20.05.2016.
  */
 public class MyNetEcho2 {
-        LinkedList<MySession> sessionList;
+    List<MySession> sessionList;
+//    LinkedList<MySession> sessionList;
+    ServerSocket serverSocket;
 
-    public MyNetEcho2() {
+    public MyNetEcho2() throws IOException {
         sessionList = new LinkedList<MySession>();
+//        sessionList = new LinkedList<MySession>();
+        serverSocket = new ServerSocket(3000);
     }
 
     public static void main(String[] args) throws IOException {
         System.out.println("MyNetEcho with 2 threads  started");
         MyNetEcho2 netEcho = new MyNetEcho2();
+        Thread regJobs = new Thread(netEcho.new RegJob(netEcho.sessionList));
+        Thread runner = new Thread(netEcho.new Runner(netEcho.sessionList));
+//        regJobs.setDaemon(true);
+        regJobs.start();
+        runner.start();
 
-        ServerSocket serverSocket = new ServerSocket(3000);
-
-//            Thread task1 = new Thread(new MyEcho1(serverSocket));
-//            Thread task2 = new Thread(new MyEcho1(serverSocket));
-//            task1.start();
-//            task2.start();
         }
+    class RegJob implements Runnable{
+        final List<MySession> sessionList;
+
+        RegJob(List<MySession> sessionList) {
+            this.sessionList = sessionList;//Collections.synchronizedList(sessionList);
+        }
+
+        @Override
+        public void run() {
+            while (true){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+
+                try {
+                    synchronized (sessionList){
+                        sessionList.add(new MySession(serverSocket.accept()));
+                    }
+                    System.out.println("in session list: "+sessionList.size());
+                } catch (IOException e) {
+//                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+
+            }
+        }
+    }
+    class Runner implements  Runnable{
+        List<MySession> sessionList;
+
+        Runner(List<MySession> sessionList) {
+            this.sessionList = sessionList; //Collections.synchronizedList(sessionList);
+        }
+
+        @Override
+        public void run() {
+            while(true){
+                synchronized (sessionList){
+                for(MySession sess: sessionList){
+//                for(Iterator<MySession> sesionIterator = sessionList.iterator(); sesionIterator.hasNext();){
+//                    MySession sess = sesionIterator.next();
+                    try {
+                        sess.step();
+//                        if(sess.step()<0){
+//                            sessionList.remove(sess);
+//                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    }
+                }
+                }
+
+            }
+        }
+    }
 
     }
 
@@ -38,19 +97,25 @@ public class MyNetEcho2 {
             this.socket = socket;
             bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
+            System.out.println("Connected "+socket.getInetAddress().toString());
             bufferedWriter.write("Hello world");
             bufferedWriter.newLine();
             bufferedWriter.flush();
         }
+
         public int step() throws IOException {
             int result = 0;
             if(bufferedReader.ready()){
+//                System.out.print(".");
                 String mes = bufferedReader.readLine();
                 if(mes.equals("quit")){
                     result--;
+                    System.out.print("I'm ready for quit");
                 }
+                System.out.print(" >"+mes);
                 bufferedWriter.write(mes);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
             }
             return result;
         }
