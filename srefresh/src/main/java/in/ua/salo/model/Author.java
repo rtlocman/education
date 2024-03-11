e07a5c22a8701adc53aab1229506ca1fa99eac4e
Sergii <rtlocman@gmail.com>
2024-03-11 16:50:33 +0300

    added equals and hashCode

--- srefresh/src/main/java/in/ua/salo/model/Author.java
+++ srefresh/src/main/java/in/ua/salo/model/Author.java
@@ -4,6 +4,7 @@ package in.ua.salo.model;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -44,6 +45,19 @@ public class Author {
                 ", lastName='" + lastName + '\'' +
 //                ", books=" + books +
                 '}' ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     public Long getId() {
