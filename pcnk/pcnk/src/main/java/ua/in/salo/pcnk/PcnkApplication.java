420ccc015c0825c734295da36dac1c47a6c369f7
Sergii <rtlocman@gmail.com>
2024-03-22 15:56:34 +0300

    added new project like pet clinic

---
+++ pcnk/pcnk/src/main/java/ua/in/salo/pcnk/PcnkApplication.java
@@ -0,0 +1,13 @@
+package ua.in.salo.pcnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcnkApplication.class, args);
+	}
+
+}
---
+++ pcnk/pcnk/src/test/java/ua/in/salo/pcnk/PcnkApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.in.salo.pcnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
