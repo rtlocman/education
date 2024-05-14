85be27f5efa083bb3f553b1ff2a4278e15e52421
Sergii <rtlocman@gmail.com>
2024-05-14 15:50:08 +0300

    chg ide

---
+++ pcnk/pcnk/pcnk_web/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ua.in.salo.pmed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
